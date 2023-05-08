4277e6bcccc29a3d92bfb19d4071bcfc50f8ae76
charles <cfb3@uw.edu>
2023-05-08 14:34:44 -0700

    Updated for Spring 2023 1.3

---
+++ app/src/main/java/edu/uw/tcss450/lab5_authentication/AuthActivity.java
@@ -0,0 +1,15 @@
+package edu.uw.tcss450.lab5_authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
